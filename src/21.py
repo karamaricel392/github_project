f0901bc5ef9dbc01fecf7b887f88dfd35c41d415
karamaricel392 <karamaricel392@gmail.com>
2025-03-30 14:42:00 +0000

    Add 21.py

---
+++ src/21.py
@@ -0,0 +1,13 @@
+import sys
+import os
+from distutils.core import setup
+
+setup(
+    name='YourProjectName',
+    version='0.1',
+    description='This is your project description.',
+    author='Author Name',
+    author_email='author@example.com',
+    packages=['your_package_name'],
+    install_requires=[],
+)

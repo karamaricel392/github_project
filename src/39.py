9425d493b75fe1d36c0ac4232ca48e39b11b7198
karamaricel392 <karamaricel392@gmail.com>
2025-05-19 14:20:32 +0000

    Add 39.py

---
+++ src/39.py
@@ -0,0 +1,8 @@
+import math
+
+def calculate_square_root(number):
+    return math.sqrt(number)
+
+number = 16
+result = calculate_square_root(number)
+print("The square root of", number, "is:", result)
